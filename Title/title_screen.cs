4609823c23e232082303445b7956316eebceee4c
SelfRef <git@selfref.dev>
2024-01-26 22:59:56 +0100

    Add popup sound

--- Title/title_screen.cs
+++ Title/title_screen.cs
@@ -5,12 +5,18 @@ public partial class title_screen : Control
 {
 	private AnimatedSprite2D _coin1;
 	private AnimatedSprite2D _coin2;
+	private Window _readyWindow;
+	private AudioStreamPlayer _audioStreamPlayer;
+	private ColorRect _popupMask;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_coin1 = GetNode<AnimatedSprite2D>("DLC/Coin1");
 		_coin2 = GetNode<AnimatedSprite2D>("DLC/Coin2");
+		_readyWindow = GetNode<Window>("ReadyWindow");
+		_audioStreamPlayer = GetNode<AudioStreamPlayer>("Play/GiveUsMoney");
+		_popupMask = GetNode<ColorRect>("PopupMask");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,5 +29,12 @@ public partial class title_screen : Control
 	private void _on_exit_pressed()
 	{
 		GetTree().Quit();
+	}
+
+	private void _on_play_pressed()
+	{
+		_readyWindow.Show();
+		_audioStreamPlayer.Play();
+		_popupMask.Show();
 	}
 }
