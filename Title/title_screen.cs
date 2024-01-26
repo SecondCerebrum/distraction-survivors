7bf1dd1f4ef8e6179cb5a96dbc591f2c1369b73f
SelfRef <git@selfref.dev>
2024-01-26 23:39:27 +0100

    Add second popup

--- Title/title_screen.cs
+++ Title/title_screen.cs
@@ -6,7 +6,9 @@ public partial class title_screen : Control
 	private AnimatedSprite2D _coin1;
 	private AnimatedSprite2D _coin2;
 	private Window _readyWindow;
-	private AudioStreamPlayer _audioStreamPlayer;
+	private Window _exitWindow;
+	private AudioStreamPlayer _giveUsMoney;
+	private AudioStreamPlayer _whereAreYouGoing;
 	private ColorRect _popupMask;
 
 	// Called when the node enters the scene tree for the first time.
@@ -15,7 +17,9 @@ public partial class title_screen : Control
 		_coin1 = GetNode<AnimatedSprite2D>("DLC/Coin1");
 		_coin2 = GetNode<AnimatedSprite2D>("DLC/Coin2");
 		_readyWindow = GetNode<Window>("ReadyWindow");
-		_audioStreamPlayer = GetNode<AudioStreamPlayer>("Play/GiveUsMoney");
+		_exitWindow = GetNode<Window>("ExitWindow");
+		_giveUsMoney = GetNode<AudioStreamPlayer>("Play/GiveUsMoney");
+		_whereAreYouGoing = GetNode<AudioStreamPlayer>("Exit/WhereAreYouGoing");
 		_popupMask = GetNode<ColorRect>("PopupMask");
 	}
 
@@ -32,8 +36,7 @@ public partial class title_screen : Control
 		var _level = GetTree().ChangeSceneToFile(level);
 	}
 
-
-	private void _on_exit_pressed()
+	private void _on_exit()
 	{
 		GetTree().Quit();
 	}
@@ -41,7 +44,26 @@ public partial class title_screen : Control
 	private void _on_play_pressed()
 	{
 		_readyWindow.Show();
-		_audioStreamPlayer.Play();
+		_giveUsMoney.Play();
 		_popupMask.Show();
+	}
+
+	private void _on_exit_pressed()
+	{
+		_exitWindow.Show();
+		_whereAreYouGoing.Play();
+		_popupMask.Show();
+	}
+
+	private void _on_ready_popup_close()
+	{
+		_readyWindow.Hide();
+		_popupMask.Hide();
+	}
+
+	private void _on_exit_popup_close()
+	{
+		_exitWindow.Hide();
+		_popupMask.Hide();
 	}
 }
